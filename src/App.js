ca6710fd50d1b5e9c7fab93225231564af51158f
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-11-12 16:25:41 +0100

    Markdown-App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react' 
+import './App.css'
+import { sampleText } from './sampleText' 
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+// creation de l'objet 'state' avec la propriétée 'text' qui charge 'sampleText'
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text') // getItem recupere text dans le localstorage 
+    if (text){ // si text existe recupere les données
+      this.setState({ text }) 
+    }else{ // sinon mais moi le 'text' par default 
+      this.setState({ text : sampleText })
+    }
+  }
+
+  // methode qui enregistre le text dans le localstorage 
+  componentDidUpdate() {
+    const {text} = this.state 
+    localStorage.setItem('text', text) // setItem enregistre une donnée
+  }
+
+
+// methode qui permet de
+  handleChange = event => {
+// event = au moment ou quelque chose change
+// target = ou et ce que cela change (le textarea)
+// value = à la valeur du textarea autrement dit de notre state
+    const text = event.target.value// recupere la valeur du state
+    this.setState({ text }) //mise a jour
+
+  }
+
+// methode qui retourne la librairie "marked"
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html}
+  }
+
+  render(){
+    return (
+      <div className="container">Bonjour
+        <div className="row" >
+          <div className="col-sm-6">
+            <textarea onChange={ this.handleChange } // rappel de la methode
+                      value={ this.state.text } // charge mon fichier 'sampleText' dans mon TextArea grace au 'state'
+                      className="form-control" 
+                      rows="35" 
+            />
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} ></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+
